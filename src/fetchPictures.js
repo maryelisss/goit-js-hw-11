4223f6c2c965f9e7832731ecf7f4cc3b431f215e
maryelisss <maryelisxd@gmail.com>
2023-03-13 16:25:26 +0200

    вже шось

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,9 +1,31 @@
-const BASE_URL = 'https://restcountries.com/v2'
-
-function fetchPictures(name) {
-    return fetch(`${BASE_URL}/name/${name}`).then(response => { if (!response.ok) {
+export default class PicturesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchPictures() {
+  const BASE_URL = 'https://pixabay.com/api'
+  const KEY = '?key=34365421-a5fa261bff150f70fc1f2b1da'
+  
+    return fetch(`${BASE_URL}/${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`).then(response => {
+      if (!response.ok) {
       throw new Error(response.status);
-    } return response.json() });
-};  
-
-export default { fetchPictures };
+    } return response.json()
+    }).then(data => {
+      this.incrementPage();
+      return data.hits
+    });
+  };   
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,52 @@
-import API from './fetchPictures.js'
-// import Notiflix, { Notify } from 'notiflix';
+import PicturesApiService from './fetchPictures.js'
+import Notiflix, { Notify } from 'notiflix';
 
 
 const searchForm = document.querySelector("#search-form");
 const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more")
+
 searchForm.addEventListener("submit", onSearch);
+loadMoreBtn.addEventListener("click", onLoadMore);
+
+const picturesApiService = new PicturesApiService();
 
 function onSearch(e) {
     e.preventDefault();
-    const searchWord = e.target.value.trim();
-    if (searchWord !== '') {
-        API.fetchPictures(searchWord).then(searchResult).catch(() => {
-            Notify.warning(`Oops, there is no country with that name`);
+    picturesApiService.query = e.currentTarget.elements.searchQuery.value;
+    picturesApiService.resetPage();
+    picturesApiService.fetchPictures()
+                .then(searchResult).catch(() => {
+            Notify.warning(`Sorry, there are no images matching your search query. Please try again.`);
             gallery.innerHTML = ``;
         });
+
+}
+
+function searchResult(array) {
+    if (array.length !== 0 ) {
+        renderGalleryCard(array);
     }
-    else {gallery.innerHTML = ``;
+    else {
+        Notify.info(`Sorry, there are no images matching your search query. Please try again.`);
     }
 }
 
-function searchResult(array) {}
+function renderGalleryCard(array) {
+        const murkup = array.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item"><b>Likes</b> ${likes}</p>
+    <p class="info-item"><b>Views</b> ${views}</p>
+    <p class="info-item"><b>Comments</b> ${comments}</p>
+    <p class="info-item"><b>Downloads</b> ${downloads}</p>
+  </div>
+</div>`
+            }).join("");
+            gallery.innerHTML = murkup;
+}
+
+function onLoadMore() {
+    picturesApiService.fetchPictures().then(renderGalleryCard);
+}
